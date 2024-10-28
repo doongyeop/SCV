84d6fa241b3b8f79aa02619fbd617d12583bed74
doongyeop <leedy977@gmail.com>
2024-10-28 16:14:24 +0900

    [back] feat: Model 기본 구조
    Entity, Controller, Repository, Service
    
    S11P31A107-153

---
+++ back/scv/src/main/java/com/scv/domain/model/repository/ModelRepository.java
@@ -0,0 +1,7 @@
+package com.scv.domain.model.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepository {
+}
---
+++ back/scv/src/main/java/com/scv/domain/model/service/ModelService.java
@@ -0,0 +1,7 @@
+package com.scv.domain.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModelService {
+}
