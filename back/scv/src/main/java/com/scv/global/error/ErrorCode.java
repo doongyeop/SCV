2d4b2ae2cdbe3ffc9705e450fab051ff3784139c
doongyeop <leedy977@gmail.com>
2024-10-28 16:09:13 +0900

    [back] feat: Model 예외 처리
    Handler, Error Code
    
    S11P31A107-153

---
+++ back/scv/src/main/java/com/scv/global/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.scv.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+    INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
+    USER_NOT_FOUND(404, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
+    MODEL_NOT_FOUND(404, "BOARD_NOT_FOUND", "모델을 찾을 수 없습니다.");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
