06788fce6a2a8ecc07b4d23f0aad8e529890f6c9
bley1217 <bley1217@gmail.com>
2024-10-30 14:53:52 +0900

    [ai] fix: .env 파일로 모든 설정 이관 완료
    S11P31A107-137

--- ai/fastapi/model_search/app.py
+++ ai/fastapi/model_search/app.py
@@ -15,11 +15,13 @@ import os
 load_dotenv(verbose=True)
 db_name = os.getenv("DB_NAME")
 collection_name = os.getenv("COLLECTION_NAME")
+milvus_host_name = os.getenv("MILVUS_HOST_NAME")
+milvus_port = os.getenv("MILVUS_PORT")
 
 app = FastAPI(root_path="/fast/v1/model/match")
 
 client = MilvusClient(
-    uri="http://localhost:19530",
+    uri="http://{}:{}".format(milvus_host_name, milvus_port),
     db_name=db_name
 )
 
