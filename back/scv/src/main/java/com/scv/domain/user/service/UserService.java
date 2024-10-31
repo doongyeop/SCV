bfbc3d0d8340b7e2af7c8814ec8daca2fd5f2b37
qwera1997 <qwera1997@naver.com>
2024-10-31 19:03:20 +0900

    [back] feat: 깃허브 리포지토리 생성 구현
    깃허브 리포지토리 생성 구현
    
    S11P31A107-234

--- back/scv/src/main/java/com/scv/domain/user/service/UserService.java
+++ back/scv/src/main/java/com/scv/domain/user/service/UserService.java
@@ -1,17 +1,25 @@
 package com.scv.domain.user.service;
 
+import com.scv.domain.oauth2.CustomOAuth2User;
 import com.scv.domain.user.domain.User;
+import com.scv.domain.user.dto.request.CreateGithubRepositoryRequestDTO;
+import com.scv.domain.user.dto.response.CommonSuccessResponseDTO;
 import com.scv.domain.user.dto.response.UserProfileResponseDTO;
 import com.scv.domain.user.exception.UserNotFoundException;
 import com.scv.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
+
+    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     private final UserRepository userRepository;
 
@@ -22,5 +30,36 @@ public class UserService {
                 .userImageUrl(user.getUserImageUrl())
                 .userNickname(user.getUserNickname())
                 .build();
+    }
+
+    public CommonSuccessResponseDTO createGithubRepository(CustomOAuth2User user, CreateGithubRepositoryRequestDTO requestDTO) {
+        String accessToken = getAccessToken(user.getName());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestBody = String.format("{\"name\":\"%s\", \"description\":\"%s 저장소\"}", requestDTO.getName(), requestDTO.getName());
+
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://api.github.com/user/repos",
+                HttpMethod.POST,
+                entity,
+                String.class
+        );
+
+        return CommonSuccessResponseDTO.builder()
+                .message("Success")
+                .build();
+    }
+
+    private String getAccessToken(String username){
+        return oAuth2AuthorizedClientService.loadAuthorizedClient("github", username)
+                .getAccessToken()
+                .getTokenValue();
     }
 }
