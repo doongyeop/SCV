05e1695c233378c46638b351ac89be2484a6043f
김현재 <nowalex322@naver.com>
2024-11-04 14:00:18 +0900

    [ai] feat: 학습된 모델 저장부분 수정중
    모델을 torch script로 저장하는 부분 작성중
    
    [#2]
    
    S11P31A107-163

--- ai/fastapi/model_train/__init__.py
+++ ai/fastapi/model_train/__init__.py
@@ -1,4 +1,4 @@
 from .model_train import ModelTrainer
-from .save_model import save_model_to_minio
+from .save_minio import save_model_to_minio
 
 __all__ = ['ModelTrainer', 'save_model_to_minio']
--- ai/fastapi/model_train/save_minio.py
+++ ai/fastapi/model_train/save_minio.py
@@ -36,7 +36,8 @@ def save_model_to_minio(model, file_name: str):
     try:
         # 모델 저장
         buffer = io.BytesIO()
-        torch.save(model, buffer)
+        torch.jit.script(model, buffer)
+        # torch.save(model, buffer)
         buffer.seek(0)
 
         # MinIO에 업로드
