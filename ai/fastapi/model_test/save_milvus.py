180b7d3d6dd9655de84e193e2aa2b29e1905633d
bley1217 <bley1217@gmail.com>
2024-11-05 09:26:48 +0900

    [ai] feat: save to milvus 함수로 이전
    S11P31A107-267

--- ai/fastapi/model_test/save_milvus.py
+++ ai/fastapi/model_test/save_milvus.py
@@ -1,21 +1,41 @@
 from dotenv import load_dotenv
 from pymilvus import connections, db, FieldSchema, MilvusClient
 import os
+import numpy as np
+import httpx
+from load_minio import load_dataset_from_minio
+from collections import defaultdict
+import torch
 
 load_dotenv(verbose=True)
-db_name = os.getenv("DB_NAME")
-collection_name = os.getenv("COLLECTION_NAME")
-milvus_host_name = os.getenv("MILVUS_HOST_NAME")
-milvus_port = os.getenv("MILVUS_PORT")
 
-client = MilvusClient(
-    uri="http://{}:{}".format(milvus_host_name, milvus_port),
-    db_name=db_name
-)
+fast_match_host_name = os.getenv("FAST_MATCH_HOST_NAME")
+fast_match_port = os.getenv("FAST_MATCH_PORT")
 
-client.load_collection(
-    collection_name=collection_name
-)
+async def save_cka_to_milvus(model, dataset, model_version_id, conv_idx, test_accuracy, layers):
+    cka_dataset = load_dataset_from_minio(dataset, "cka")
+# Milvus CKA 저장
+    cka_matrix = defaultdict(list)
+    with torch.no_grad():
+        for index, (input, label) in enumerate(cka_dataset):
 
-def save_cka_to_milvus():
-    
+            x = input
+            
+            for i in range(0, len(model)):
+                x = model[i](x)
+                if i in conv_idx:
+                    cka_matrix[i].append(torch.flatten(x).cpu().numpy())
+                    
+
+    for i in cka_matrix.keys():
+        mat = np.array(cka_matrix[i])
+        cka = (mat @ mat.T).flatten()
+        cka_vec = cka / np.linalg.norm(cka)
+        async with httpx.AsyncClient() as client:
+            res = await client.post(f"http://{fast_match_host_name}:{fast_match_port}/fast/v1/model/match/{model_version_id}/{i}",
+                        json={
+                            "test_accuracy": test_accuracy,
+                            "layers" : layers,
+                            "cka_vec": cka_vec.tolist()
+                        })
+            print(res)
