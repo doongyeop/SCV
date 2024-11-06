3428c02ae44e14fdbfedc639a436ce79f8a98868
qwera1997 <qwera1997@naver.com>
2024-11-06 15:24:39 +0900

    [back] feat: 유틸 클래스 빈 등록 삭제
    유틸 클래스 빈 등록 삭제
    
    S11P31A107-246

--- back/scv/src/main/java/com/scv/global/util/JwtUtil.java
+++ back/scv/src/main/java/com/scv/global/util/JwtUtil.java
@@ -1,88 +1,82 @@
 package com.scv.global.util;
 
+import com.scv.domain.oauth2.CustomOAuth2User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Date;
 
-@Slf4j
-@Component
 public class JwtUtil {
 
-    private final String ACCESS_TOKEN_NAME;
-    private final String REFRESH_TOKEN_NAME;
-    private final byte[] ACCESS_TOKEN_SECRET_KEY_BYTES;
-    private final byte[] REFRESH_TOKEN_SECRET_KEY_BYTES;
-    private final int ACCESS_TOKEN_EXPIRATION;
-    private final int REFRESH_TOKEN_EXPIRATION;
+    public static final String ACCESS_TOKEN_NAME = System.getenv("JWT_ACCESS_NAME");
+    public static final String REFRESH_TOKEN_NAME = System.getenv("JWT_REFRESH_NAME");
+    public static final byte[] ACCESS_TOKEN_SECRET_KEY_BYTES = System.getenv("JWT_ACCESS_KEY").getBytes(StandardCharsets.UTF_8);
+    public static final byte[] REFRESH_TOKEN_SECRET_KEY_BYTES = System.getenv("JWT_REFRESH_KEY").getBytes(StandardCharsets.UTF_8);
+    public static final int ACCESS_TOKEN_EXPIRATION = Integer.parseInt(System.getenv("JWT_ACCESS_EXPIRATION"));
+    public static final int REFRESH_TOKEN_EXPIRATION = Integer.parseInt(System.getenv("JWT_REFRESH_EXPIRATION"));
 
-    public JwtUtil(@Value("${spring.jwt.token.access.name}") String ACCESS_TOKEN_NAME,
-                   @Value("${spring.jwt.token.refresh.name}") String REFRESH_TOKEN_NAME,
-                   @Value("${spring.jwt.token.access.secret-key}") String ACCESS_TOKEN_SECRET_KEY,
-                   @Value("${spring.jwt.token.refresh.secret-key}") String REFRESH_TOKEN_SECRET_KEY,
-                   @Value("${spring.jwt.token.access.expiration}") int ACCESS_TOKEN_EXPIRATION,
-                   @Value("${spring.jwt.token.refresh.expiration}") int REFRESH_TOKEN_EXPIRATION) {
-        this.ACCESS_TOKEN_NAME = ACCESS_TOKEN_NAME;
-        this.REFRESH_TOKEN_NAME = REFRESH_TOKEN_NAME;
-        this.ACCESS_TOKEN_SECRET_KEY_BYTES = ACCESS_TOKEN_SECRET_KEY.getBytes(StandardCharsets.UTF_8);
-        this.REFRESH_TOKEN_SECRET_KEY_BYTES = REFRESH_TOKEN_SECRET_KEY.getBytes(StandardCharsets.UTF_8);
-        this.ACCESS_TOKEN_EXPIRATION = ACCESS_TOKEN_EXPIRATION;
-        this.REFRESH_TOKEN_EXPIRATION = REFRESH_TOKEN_EXPIRATION;
-    }
-
-    private String createToken(String userUuid, String type, byte[] secretKeyBytes, long expiration) {
+    public static String createAccessToken(CustomOAuth2User user) {
         return Jwts.builder()
-                .setSubject(userUuid)
-                .claim("type", type)
+                .setSubject(user.getUserId().toString())
+                .claim("userUuid", user.getUserUuid())
+                .claim("userNickname", user.getUserNickname())
+                .claim("userRepo", user.getUserRepo())
                 .setIssuedAt(Date.from(ZonedDateTime.now(ZoneOffset.UTC).toInstant()))
-                .setExpiration(Date.from(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(expiration).toInstant()))
-                .signWith(Keys.hmacShaKeyFor(secretKeyBytes), SignatureAlgorithm.HS256)
+                .setExpiration(Date.from(ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(ACCESS_TOKEN_EXPIRATION).toInstant()))
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET_KEY_BYTES), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String createAccessToken(String userUuid) {
-        return createToken(userUuid, ACCESS_TOKEN_NAME, ACCESS_TOKEN_SECRET_KEY_BYTES, ACCESS_TOKEN_EXPIRATION);
+    public static String createRefreshToken(CustomOAuth2User user) {
+        return Jwts.builder()
+                .setSubject(user.getUserId().toString())
+                .setIssuedAt(Date.from(ZonedDateTime.now(ZoneOffset.UTC).toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(REFRESH_TOKEN_EXPIRATION).toInstant()))
+                .signWith(Keys.hmacShaKeyFor(REFRESH_TOKEN_SECRET_KEY_BYTES), SignatureAlgorithm.HS256)
+                .compact();
     }
 
-    public String createRefreshToken(String userUuid) {
-        return createToken(userUuid, REFRESH_TOKEN_NAME, REFRESH_TOKEN_SECRET_KEY_BYTES, REFRESH_TOKEN_EXPIRATION);
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKeyResolver(new SigningKeyResolverAdapter() {
-                        @Override
-                        public byte[] resolveSigningKeyBytes(JwsHeader header, Claims claims) {
-                            String type = claims.get("type", String.class);
-                            return type.equals(ACCESS_TOKEN_NAME) ? ACCESS_TOKEN_SECRET_KEY_BYTES : REFRESH_TOKEN_SECRET_KEY_BYTES;
-                        }
-                    })
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.error("Token expired: {}", e.getMessage());
-        } catch (SecurityException | MalformedJwtException e) {
-            log.error("Invalid token signature or malformed token: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.error("Unsupported token: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.error("Illegal argument provided for token: {}", e.getMessage());
-        }
-
+    public static boolean isAccessTokenTampered(String token) {
         return false;
     }
 
-    public String getUserUuid(String accessToken) {
-        return Jwts.parserBuilder().setSigningKey(ACCESS_TOKEN_SECRET_KEY_BYTES).build()
-                .parseClaimsJws(accessToken).getBody().getSubject();
+    public static boolean isRefreshTokenTampered(String token) {
+        return false;
     }
+
+    public static boolean isAccessTokenNoneAlgorithmUsed(String token) {
+        return false;
+    }
+
+    public static boolean isRefreshTokenNoneAlgorithmUsed(String token) {
+        return false;
+    }
+
+    public static boolean isAccessTokenExpired(String token) {
+        return false;
+    }
+
+    public static boolean isRefreshTokenExpired(String token) {
+        return false;
+    }
+
+    public static String reIssueAccessToken(String token) {
+        return "";
+    }
+
+    public static String reIssueRefreshToken(String token) {
+        return "";
+    }
+
+    public static Claims parseAccessTokenClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET_KEY_BYTES))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
 }
