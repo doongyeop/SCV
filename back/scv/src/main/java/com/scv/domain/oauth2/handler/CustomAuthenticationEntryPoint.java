e15bc9eb5a064034c7e33b2d3b464949d1a1c2e0
qwera1997 <qwera1997@naver.com>
2024-11-07 09:44:22 +0900

    [back] feat: 예외 처리 핸들러 스웨거 추가
    예외 처리 핸들러 스웨거 추가
    
    S11P31A107-239

--- back/scv/src/main/java/com/scv/domain/oauth2/handler/CustomAuthenticationEntryPoint.java
+++ back/scv/src/main/java/com/scv/domain/oauth2/handler/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.scv.domain.oauth2.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scv.global.error.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -8,11 +10,24 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static com.scv.global.error.ErrorCode.INVALID_TOKEN;
+
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        String url = request.getServletPath();
+
+        if (url.startsWith("/swagger-ui") || url.startsWith("/v3/api-docs")) {
+            response.sendRedirect("/oauth2/authorization/github");
+            return;
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(INVALID_TOKEN);
+        response.setStatus(INVALID_TOKEN.getHttpStatus());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
