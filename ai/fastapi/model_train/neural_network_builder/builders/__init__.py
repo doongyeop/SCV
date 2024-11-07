6b45c8ba0d8f598328c8a2d0ab086d38c02cd386
bley1217 <bley1217@gmail.com>
2024-11-07 17:49:07 +0900

    [ai] chore: neural_network_builder 폴더 복사

---
+++ ai/fastapi/model_train/neural_network_builder/builders/__init__.py
@@ -0,0 +1,5 @@
+from .layer_builder import LayerBuilder
+from .model_builder import ModelBuilder
+from .model_code_generator import ModelCodeGenerator
+
+__all__ = ['LayerBuilder', 'ModelBuilder', 'ModelCodeGenerator']
