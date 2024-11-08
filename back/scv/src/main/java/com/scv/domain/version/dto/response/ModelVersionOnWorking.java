43fd17b5ae65cf446f87133ca482d8ccaaa82219
doongyeop <leedy977@gmail.com>
2024-11-08 16:09:54 +0900

    [back] fix: 제작중인 모델 response 변경
    ModelVersionOnWorking 유저 프로필 삭제
    수정날짜
    
    S11P31A107-280

--- back/scv/src/main/java/com/scv/domain/version/dto/response/ModelVersionOnWorking.java
+++ back/scv/src/main/java/com/scv/domain/version/dto/response/ModelVersionOnWorking.java
@@ -7,23 +7,23 @@ import com.scv.domain.version.domain.ModelVersion;
 import java.time.LocalDateTime;
 
 public record ModelVersionOnWorking(
-        UserProfileResponseDTO userProfile,
         String title,
         Long modelVersionId,
         int version,
         DataSet dataName,
         double accuracy,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) {
     public ModelVersionOnWorking(ModelVersion modelVersion) {
         this(
-                new UserProfileResponseDTO(modelVersion.getModel().getUser()),
                 modelVersion.getModel().getName(),
                 modelVersion.getId(),
                 modelVersion.getVersionNo(),
                 modelVersion.getModel().getData().getName(),
                 modelVersion.getResult() != null ? modelVersion.getResult().getTestAccuracy() : 0.0,
-                modelVersion.getCreatedAt()
+                modelVersion.getCreatedAt(),
+                modelVersion.getUpdatedAt()
         );
     }
 }
