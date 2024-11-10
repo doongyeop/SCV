5ab6e7b9a354860d6249fd3cbcccd325297f2dae
qwera1997 <qwera1997@naver.com>
2024-11-11 00:25:14 +0900

    [back] refactor: 에러 코드 정리
    에러 코드 정리
    
    S11P31A107-351

--- back/scv/src/main/java/com/scv/global/error/ErrorCode.java
+++ back/scv/src/main/java/com/scv/global/error/ErrorCode.java
@@ -13,10 +13,10 @@ public enum ErrorCode {
     GITHUB_API_NOT_FOUND(404, "GITHUB_API_NOT_FOUND", "Resource not found"),
     GITHUB_API_CONFLICT(409, "GITHUB_API_CONFLICT", "Conflict"),
     GITHUB_API_UNPROCESSABLE_ENTITY(422, "GITHUB_API_UNPROCESSABLE_ENTITY", "Validation failed, or the endpoint has been spammed."),
-
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+
     RESULT_NOT_FOUND(404, "RESULT_NOT_FOUND", "결과를 찾을 수 없습니다."),
     DATA_NOT_FOUND(404, "DATA_NOT_FOUND", "데이터를 찾을 수 없습니다."),
     MODEL_VERSION_NOT_FOUND(404, "MODEL_VERSION_NOT_FOUND", "버전을 찾을 수 없습니다."),
