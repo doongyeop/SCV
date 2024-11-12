be7e694777a416c9e282eff040d7067e2b0e6b76
qwera1997 <qwera1997@naver.com>
2024-11-12 20:02:04 +0900

    [back] feat: GithubService 공통 로직 수정 및 DI 최적화
    GithubService 공통 로직 수정 및 DI 최적화
    
    S11P31A107-390

--- back/scv/src/main/java/com/scv/domain/user/service/GithubService.java
+++ back/scv/src/main/java/com/scv/domain/user/service/GithubService.java
@@ -1,34 +1,43 @@
 package com.scv.domain.user.service;
 
 import com.scv.domain.data.enums.DataSet;
+import com.scv.domain.user.dto.request.CreateGithubRepoApiRequestDTO;
+import com.scv.domain.user.dto.request.ExportGithubRepoBlockFileApiRequestDTO;
+import com.scv.domain.user.dto.request.ExportGithubRepoBlockFileRequestDTO;
 import com.scv.domain.user.dto.request.LinkGithubRepoRequestDTO;
 import com.scv.domain.user.dto.response.GithubEmailApiResponseDTO;
 import com.scv.domain.user.dto.response.GithubRepoApiResponseDTO;
+import com.scv.domain.user.dto.response.GithubRepoBlockFileApiResponseDTO;
 import com.scv.domain.user.exception.UserNotFoundException;
 import com.scv.global.jwt.service.RedisTokenService;
 import com.scv.global.jwt.util.JwtUtil;
 import com.scv.global.oauth2.auth.CustomOAuth2User;
-import com.scv.domain.user.dto.request.CommitGithubRepoFileRequestDTO;
-import com.scv.domain.user.dto.response.GithubRepoFileResponseDTO;
+import com.scv.domain.user.dto.response.GithubRepoBlockFileResponseDTO;
 import com.scv.domain.user.exception.GithubConflictException;
 import com.scv.domain.user.exception.GithubNotFoundException;
 import com.scv.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class GithubService {
 
     private final GithubApiService githubApiService;
     private final RedisTokenService redisTokenService;
-
     private final UserRepository userRepository;
+
+    public GithubService(@Qualifier("githubApiRestClientService") GithubApiService githubApiService, RedisTokenService redisTokenService, UserRepository userRepository) {
+        this.githubApiService = githubApiService;
+        this.redisTokenService = redisTokenService;
+        this.userRepository = userRepository;
+    }
 
     // 깃허브에서 primary email 을 조회하는 메서드
     public String getGithubPrimaryEmail(String accessToken) {
@@ -45,12 +54,14 @@ public class GithubService {
             throw GithubConflictException.getInstance();
         }
 
-        githubApiService.createGithubRepo(authUser, requestDTO);
+        CreateGithubRepoApiRequestDTO newRequestDTO = CreateGithubRepoApiRequestDTO.builder()
+                .name(requestDTO.getRepoName())
+                .description(getNewGithubRepoDescription(requestDTO.getRepoName()))
+                .build();
+        githubApiService.createGithubRepo(authUser, newRequestDTO);
 
         userRepository.updateUserRepoById(authUser.getUserId(), requestDTO.getRepoName());
-
         redisTokenService.addToBlacklist(accessToken);
-
         return JwtUtil.createAccessToken(userRepository.findById(authUser.getUserId()).orElseThrow(UserNotFoundException::getInstance));
     }
 
@@ -61,10 +72,37 @@ public class GithubService {
         }
 
         userRepository.updateUserRepoById(authUser.getUserId(), requestDTO.getRepoName());
-
         redisTokenService.addToBlacklist(accessToken);
-
         return JwtUtil.createAccessToken(userRepository.findById(authUser.getUserId()).orElseThrow(UserNotFoundException::getInstance));
+    }
+
+    // 깃허브에서 모델 import 서비스 로직
+    public GithubRepoBlockFileResponseDTO importGithubRepoBlockFile(CustomOAuth2User auth2User, DataSet dataName, String modelName) {
+        return GithubRepoBlockFileResponseDTO.builder()
+                .content(githubApiService.importGithubRepoBlockFile(auth2User, dataName, modelName)
+                        .map(GithubRepoBlockFileApiResponseDTO::getContent)
+                        .map(encodedContent -> new String(Base64.getDecoder().decode(encodedContent.replaceAll("\\s", ""))))
+                        .orElse(null))
+                .build();
+    }
+
+    // 깃허브에 모델 export 서비스 로직
+    public void exportGithubRepoBlockFile(CustomOAuth2User auth2User, ExportGithubRepoBlockFileRequestDTO requestDTO) {
+        String sha = githubApiService.importGithubRepoBlockFile(auth2User, requestDTO.getDataName(), requestDTO.getModelName())
+                .map(GithubRepoBlockFileApiResponseDTO::getSha)
+                .orElse(null);
+
+        ExportGithubRepoBlockFileApiRequestDTO newRequestDTO = ExportGithubRepoBlockFileApiRequestDTO.builder()
+                .content(Base64.getEncoder().encodeToString(requestDTO.getContent().getBytes(StandardCharsets.UTF_8)))
+                .message(getExportMessage(requestDTO.getMessage(), requestDTO.getModelName(), requestDTO.getVersionNo(), sha))
+                .sha(sha)
+                .build();
+        githubApiService.exportGithubRepoBlockFile(auth2User, requestDTO.getDataName(), requestDTO.getModelName(), newRequestDTO);
+    }
+
+    // 깃허브 새 리포 설명 메시지 반환
+    private String getNewGithubRepoDescription(String repoName) {
+        return "Welcome " + repoName + "!";
     }
 
     // 깃허브에서 Repository 리스트의 이름들을 Set 으로 반환
@@ -74,16 +112,11 @@ public class GithubService {
                 .collect(Collectors.toSet());
     }
 
-    // 깃허브에서 모델 import 서비스 로직
-    public GithubRepoFileResponseDTO importGithubRepoFile(CustomOAuth2User auth2User, DataSet dataName, String modelName) {
-        return GithubRepoFileResponseDTO.builder()
-                .content(githubApiService.getContentFromGithubRepositoryFile(auth2User, dataName, modelName))
-                .build();
-    }
-
-    // 깃허브에 모델 export 서비스 로직
-    public void exportGithubRepoFile(CustomOAuth2User auth2User, CommitGithubRepoFileRequestDTO requestDTO) {
-        githubApiService.commitGithubRepositoryFile(auth2User, requestDTO);
+    // 깃허브에 모델 export 시 커밋 메시지 생성
+    private String getExportMessage(String message, String modelName, Long versionNo, String sha) {
+        if (message != null && !message.trim().isEmpty()) return message;
+        if (sha == null) return "feat: [" + modelName + " v-" + versionNo + "] - SCV";
+        return "refactor: [" + modelName + " v-" + versionNo + "] - SCV";
     }
 
 }
