3d32baab30a0ee6eb7039751e4870a126f6b9b10
김현재 <nowalex322@naver.com>
2024-11-12 19:47:22 +0900

    [ai] refactor: neural_network_builder 옮기기
    S11P31A107-383

--- ai/fastapi/model_test/neural_network_builder/builders/__init__.py
+++
@@ -1,5 +0,0 @@
-from .layer_builder import LayerBuilder
-from .model_builder import ModelBuilder
-from .model_code_generator import ModelCodeGenerator
-
-__all__ = ['LayerBuilder', 'ModelBuilder', 'ModelCodeGenerator']
