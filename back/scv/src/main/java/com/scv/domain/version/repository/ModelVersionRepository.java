1f1b72f59ed247326f0229d5228ab88bb3e5e8b2
doongyeop <leedy977@gmail.com>
2024-11-13 14:17:31 +0900

    [back] refactor: ModelService N+1 개선
    S11P31A107-372

--- back/scv/src/main/java/com/scv/domain/version/repository/ModelVersionRepository.java
+++ back/scv/src/main/java/com/scv/domain/version/repository/ModelVersionRepository.java
@@ -4,24 +4,27 @@ import com.scv.domain.version.domain.ModelVersion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ModelVersionRepository extends JpaRepository<ModelVersion, Long> {
 
     List<ModelVersion> findAllByModel_IdAndDeletedFalse(Long id);
 
-//    Optional<ModelVersion> findByIdAndDeletedFalse(Long id);
 
     @Query("SELECT mv FROM ModelVersion mv JOIN mv.model m WHERE m.user.userId = :userId AND mv.isWorkingOn = true AND mv.deleted = false")
     Page<ModelVersion> findAllByUserAndIsWorkingTrueAndDeletedFalse(@Param("userId") Long userId, Pageable pageable);
 
-
+    @Modifying
+    @Transactional
+    @Query("UPDATE ModelVersion mv SET mv.deleted = true WHERE mv.model.id = :modelId AND mv.deleted = false")
+    void softDeleteAllByModelId(@Param("modelId") Long modelId);
 
 
 }
