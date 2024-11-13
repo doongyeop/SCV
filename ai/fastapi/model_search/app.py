0df90f4869b8846f687e5e94592b51819e71998b
bley1217 <bley1217@gmail.com>
2024-11-13 17:38:40 +0900

    [ai] feat: gpu deployment 작성중
    S11P31A107-397

--- ai/fastapi/model_search/app.py
+++ ai/fastapi/model_search/app.py
@@ -16,15 +16,15 @@ import redis
 load_dotenv(verbose=True)
 db_name = os.getenv("DB_NAME")
 collection_name = os.getenv("COLLECTION_NAME")
-milvus_host_name = os.getenv("MILVUS_HOST_NAME")
-milvus_port = os.getenv("MILVUS_PORT")
+# milvus_host_name = os.getenv("MILVUS_HOST_NAME")
+# milvus_port = os.getenv("MILVUS_PORT")
 redis_host_name = os.getenv("REDIS_HOST_NAME")
 redis_port = os.getenv("REDIS_PORT")
 
 app = FastAPI(root_path="/fast/v1/model/match")
 
 client = MilvusClient(
-    uri="http://{}:{}".format(milvus_host_name, milvus_port),
+    uri="./milvus_demo.db",
     db_name=db_name
 )
 
