00f421267de527c8dc88d83b30617c7147cd6d40
doongyeop <leedy977@gmail.com>
2024-11-14 10:06:32 +0900

    [back] refactor: restTemplate url 변경
    url 호스트, 포트 변수로 관리
    
    S11P31A107-374

--- back/scv/src/main/java/com/scv/global/util/UrlUtil.java
+++ back/scv/src/main/java/com/scv/global/util/UrlUtil.java
@@ -18,12 +18,12 @@ public class UrlUtil {
     @Value("${spring.fastapi.test.port}")
     private String fastTestPort;
 
-    //    String url = "http://localhost:8003/fast/v1/models/" + modelVersion.getModel().getId() + "/versions/" + modelVersionId;
+//    "http://host:port/fast/v1/models/" + modelVersion.getModel().getId() + "/versions/" + modelVersionId;
     public String getTrainUrl(Long modelId, Long modelVersionId) {
         return String.format("http://%s:%s/fast/v1/models/%d/versions/%d", fastTrainHost, fastTrainPort, modelId, modelVersionId);
     }
 
-    //    String url = "http://localhost:8002/fast/v1/model/test/analyze/" + modelId + "/" + modelVersionId + "/" + data.toLowerCase();
+//    "http://host:port/fast/v1/model/test/analyze/" + modelId + "/" + modelVersionId + "/" + data.toLowerCase();
     public String getTestUrl(Long modelId, Long modelVersionId, String dataName) {
         return String.format("http://%s:%s/fast/v1/model/test/analyze/%d/%d/%s", fastTestHost, fastTestPort, modelId, modelVersionId, dataName.toLowerCase());
     }
