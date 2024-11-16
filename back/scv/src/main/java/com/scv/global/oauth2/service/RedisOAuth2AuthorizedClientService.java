d54859dd93a88862140c194fb6f1b87c51df3d7c
qwera1997 <qwera1997@naver.com>
2024-11-16 20:04:07 +0900

    [back] feat: memory에서 redis로 토큰 저장소 변경
    memory에서 redis로 토큰 저장소 변경
    
    S11P31A107-421

---
+++ back/scv/src/main/java/com/scv/global/oauth2/service/RedisOAuth2AuthorizedClientService.java
@@ -0,0 +1,45 @@
+package com.scv.global.oauth2.service;
+
+import com.scv.global.util.AESUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisOAuth2AuthorizedClientService implements OAuth2AuthorizedClientService {
+
+    public RedisOAuth2AuthorizedClientService(@Qualifier("oauthMasterTemplate") RedisTemplate<String, Object> oauthRedisMasterTemplate) {
+        this.oauthRedisMasterTemplate = oauthRedisMasterTemplate;
+    }
+
+    private final RedisTemplate<String, Object> oauthRedisMasterTemplate;
+
+    private String generateKey(String clientRegistrationId, String principalName) {
+        return String.format("%s:%s", clientRegistrationId, principalName);
+    }
+
+    public String getAccessToken(String clientRegistrationId, String principalName) {
+        String key = generateKey(clientRegistrationId, principalName);
+        return AESUtil.decrypt((String) oauthRedisMasterTemplate.opsForValue().get(key));
+    }
+
+    @Override
+    public <T extends OAuth2AuthorizedClient> T loadAuthorizedClient(String clientRegistrationId, String principalName) {
+        return null;
+    }
+
+    @Override
+    public void saveAuthorizedClient(OAuth2AuthorizedClient authorizedClient, Authentication principal) {
+        String key = generateKey(authorizedClient.getClientRegistration().getRegistrationId(), principal.getName());
+        oauthRedisMasterTemplate.opsForValue().set(key, AESUtil.encrypt(authorizedClient.getAccessToken().getTokenValue()));
+    }
+
+    @Override
+    public void removeAuthorizedClient(String clientRegistrationId, String principalName) {
+        String key = generateKey(clientRegistrationId, principalName);
+        oauthRedisMasterTemplate.delete(key);
+    }
+}
