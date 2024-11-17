61ede1920abee57026c881cc51020ec47514f59f
qwera1997 <qwera1997@naver.com>
2024-11-17 14:34:25 +0900

    [back] refactor: oauth token 삭제 로직 추가
    oauth token 삭제 로직 추가
    
    S11P31A107-424

--- back/scv/src/main/java/com/scv/global/jwt/filter/CustomJwtLogoutFilter.java
+++ back/scv/src/main/java/com/scv/global/jwt/filter/CustomJwtLogoutFilter.java
@@ -2,6 +2,8 @@ package com.scv.global.jwt.filter;
 
 import com.scv.global.jwt.service.RedisTokenService;
 import com.scv.global.jwt.util.CookieUtil;
+import com.scv.global.jwt.util.JwtUtil;
+import com.scv.global.oauth2.service.RedisOAuth2AuthorizedClientService;
 import com.scv.global.util.ResponseUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,6 +23,7 @@ import static com.scv.global.jwt.util.JwtUtil.REFRESH_TOKEN_NAME;
 public class CustomJwtLogoutFilter extends OncePerRequestFilter {
 
     private final RedisTokenService redisTokenService;
+    private final RedisOAuth2AuthorizedClientService redisOAuth2AuthorizedClientService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -32,6 +35,9 @@ public class CustomJwtLogoutFilter extends OncePerRequestFilter {
 
             CookieUtil.getCookie(request, REFRESH_TOKEN_NAME)
                     .ifPresent(cookie -> redisTokenService.deleteFromWhitelist(cookie.getValue()));
+
+            CookieUtil.getCookie(request, REFRESH_TOKEN_NAME)
+                    .ifPresent(cookie -> redisOAuth2AuthorizedClientService.removeAuthorizedClient("github", JwtUtil.parseRefreshTokenClaims(cookie.getValue()).getSubject()));
 
             CookieUtil.deleteCookie(response, ACCESS_TOKEN_NAME);
             CookieUtil.deleteCookie(response, REFRESH_TOKEN_NAME);
